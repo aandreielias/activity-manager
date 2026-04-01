858ac827d680be7485c96e678693477c60261044
aandreielias <aandreielias@gmail.com>
2026-04-01 11:34:20 +0000

    Deploying to gh-pages from @ aandreielias/activity-manager@60abf6c9f847aaeebc03e3065aaf1193d92908cd 🚀

---
+++ assets/config-C-GAOvf2.js
@@ -0,0 +1,1 @@
+var e={URL:`https://kmsdsymoehleonxzcbnm.supabase.co`,ANON_KEY:`eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imttc2RzeW1vZWhsZW9ueHpjYm5tIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQ5NTg3NjAsImV4cCI6MjA5MDUzNDc2MH0.Z0eznsy0BBFpwHKtlXBVNk5M8Yc_saXEpYB-DH7yt0g`};export{e as t};
